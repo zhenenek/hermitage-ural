2c5fcaa19753f400771c533dd0daf6dd907b6f0f
zhenenek <zhenenek@gmail.com>
2025-02-23 18:48:51 +0300

    +extract_answer

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import ollama
-import sqlite3
 from edit import out_txt
 from prompt import prompt
 from bd import save_to_db  
 
 client = ollama.Client()
-
 model = "deepseek-r1:14b"
 
 model_reply = client.generate(model=model, prompt=prompt)
 result = model_reply.response
 
-print('Response:',result)
-save_to_db(result)
+def extract_answer(result):
+    parts = result.split("</think>")
+    
+    if len(parts) > 1:
+        return parts[1].strip()
+    
+    
+    return result.strip()
+
+clean_response = extract_answer(result)
+save_to_db(clean_response)
+print(clean_response)
